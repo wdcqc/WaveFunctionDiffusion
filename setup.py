1a60e6ecea3aff6130490400ec9a4dfedaffd545
wdcqc <119406052+wdcqc@users.noreply.github.com>
2023-01-05 09:57:06 +0800

    Nineth Commit

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='wfd',
@@ -8,7 +8,7 @@ setup(
     author='wdcqc',
     author_email='119406052+wdcqc@users.noreply.github.com',
     license='creativeml-openrail-m',
-    packages=['wfd'],
+    packages=find_packages("."),
     install_requires=[
         "accelerate",
         "bitsandbytes",
